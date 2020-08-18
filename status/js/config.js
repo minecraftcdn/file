48d4d9645bf08e2121a93ac6e7f84d174df6de9c
minecraftcdn <minecraftcdn@lem0n.me>
2020-08-18 13:01:11 +0800

    “update”

---
+++ status/js/config.js
@@ -0,0 +1,7 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'u887095-d97f4f9afcb995593b516ad9', 
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
